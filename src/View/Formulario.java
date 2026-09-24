47119421489fe5ea9eaa8e052e28d399efb09c15
LAB55 <lab55@fatec.com>
2026-09-24 20:59:03 -0300

    feat: Cria labels

--- src/View/Formulario.java
+++ src/View/Formulario.java
@@ -24,6 +24,24 @@ public class Formulario {
         form.setLayout(null);
         Container painelDeConteudo = form.getContentPane();
 
+        //labels
+        lblValor1 = new JLabel("Valor Inicial:");
+        lblValor1.setBounds(50, 30, 80, 25);
+        form.add(lblValor1);
+
+        lblValor2 = new JLabel("Prazo:");
+        lblValor2.setBounds(50, 70, 80, 25);
+        form.add(lblValor2);
+
+        lblValor3 = new JLabel("Investimento");
+        lblValor3.setBounds(50, 110, 80, 25);
+        form.add(lblValor3);
+
+        lblResultado = new JLabel("Resultado ");
+        lblResultado.setBounds(50, 150, 80, 25);
+        form.add(lblResultado);
+
+
 
 
     }
